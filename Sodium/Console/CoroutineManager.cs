134f9ef35735d36dd587104ff4208a6d534076e0
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-09-14 12:18:18 -0400

    Rename Administrator to Console and update

--- Sodium/Console/CoroutineManager.cs
+++ Sodium/Console/CoroutineManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace Console
+namespace Sodium.Console
 {
     public class CoroutineManager : MonoBehaviour // Thanks to ShibaGT for helping with the coroutines
     {
