f7a46f41240fa59d037ac9dbf709871eb05fec04
Juan Searn Leoson <tagmonkebusiness@gmail.com>
2025-09-21 17:29:22 -0400

    final 1.6.1

--- Sodium/PluginInfo.cs
+++ Sodium/PluginInfo.cs
@@ -5,7 +5,8 @@
         public const string GUID = "0000.com.SodiumDevelopment.Sodium"; // 0 at beginning of UUID to run first in BepInEx load order
         public const string Name = "Sodium";
         public const string Description = "Performance Mod for Gorilla Tag";
-        public const string Version = "1.7.0";
+        public const string Version = "1.6.1";
         public const string ConsoleVersion = "Sodium" + Version;
+        public const string WriteAccessKey = "803b607b8cdd5571853047da83a41d5f522f52a290e530d580c70d5e8c08904e_" + Version;
     }
 }
